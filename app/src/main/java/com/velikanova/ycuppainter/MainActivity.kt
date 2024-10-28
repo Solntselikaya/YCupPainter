474498726b30a9701e590fe14f26ac5c6a4c6ea2
Sofiya Velikanova <solntselikaya.sofia@yandex.ru>
2024-10-28 23:28:52 +0700

    Add colors and background

--- app/src/main/java/com/velikanova/ycuppainter/MainActivity.kt
+++ app/src/main/java/com/velikanova/ycuppainter/MainActivity.kt
@@ -3,6 +3,7 @@ package com.velikanova.ycuppainter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -22,25 +23,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Canvas(
+
+                    ) { }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    YCupPainterTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/velikanova/ycuppainter/ui/theme/Color.kt
+++ app/src/main/java/com/velikanova/ycuppainter/ui/theme/Color.kt
@@ -2,6 +2,14 @@ package com.velikanova.ycuppainter.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val Gray = Color(0xFF8B8B8B)
+val Green = Color(0xFFA8DB10)
+val Blue = Color(0xFF1976D2)
+val Orange = Color(0xFFFF3D00)
+
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
